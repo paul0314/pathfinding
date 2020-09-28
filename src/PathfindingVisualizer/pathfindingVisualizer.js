58a3e291cbcb576274453a77249452a04e77a066
PaulV <s6pavoel@uni-bonn.de>
2020-09-28 23:20:30 +0200

    fixed navbar issue, dropdown menus

--- src/PathfindingVisualizer/pathfindingVisualizer.js
+++ src/PathfindingVisualizer/pathfindingVisualizer.js
@@ -1,10 +1,15 @@
-const dropdown = document.querySelectorAll(".dropdown-toggle");
+const dropdownBtns = document.querySelectorAll(".dropdown-toggle");
+const dropdownContent = document.querySelectorAll(".dropdown-content");
 
-dropdown.forEach(function (item) {
+dropdownBtns.forEach(function (item) {
     item.addEventListener("click", function (e) {
         const parent = e.currentTarget.parentElement;
-        console.log(parent);
         const content = parent.querySelector(".dropdown-content");
+        dropdownContent.forEach(function (contents) {
+            if(contents !== content){
+                contents.classList.remove("show-dropdown");
+            }
+        });
         content.classList.toggle("show-dropdown");
     });
 });
